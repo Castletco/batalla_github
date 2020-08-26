b97a97a2e1979b6306c3d801f6b8e62a190f57d7
DiegoCastillo <diegocastillonaranjo@gmail.com>
2020-08-25 20:20:25 -0400

    modificación para batalla github

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <table>
+      <h2>Player 1</h2>
+      <tr>
+        <td>Username: </td>
+        <td>{{ info.login }}</td>
+      </tr>
+
+      <tr>
+        <td>Public repositories: </td>
+        <td>{{ info.public_repos }}</td>
+      </tr>
+
+      <tr>
+        <td>Number of followers: </td>
+        <td>{{ info.followers }}</td>
+      </tr>
+
+      <tr>
+        <td>Number Following: </td>
+        <td>{{ info.following }}</td>
+      </tr>
+
+      <tr>
+        <td>Public Gists: </td>
+        <td>{{ info.public_gists }}</td>
+      </tr>
+
+      <tr>
+        <td><strong>Total Score: </strong></td>
+        <td><strong>{{ info.public_repos + info.followers + info.following + info.public_gists}}</strong></td>
+      </tr>
+    </table>
+    <p>The result of our request: {{ info }}</p>
   </div>
 </template>
 
@@ -37,6 +42,25 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    this.getBerryData();
+  },
+  methods: {
+    async getBerryData() {
+      try {
+        let response = await this.$http.get("https://api.github.com/users/Castletco");
+        this.info = response.data;      // a lot more information comes back in the response, so for now we only want the actual data of the response
+      }
+      catch(error) {
+        console.log(`Something went wrong: ${error}`);
+      }
+    }
   }
 }
 </script>
@@ -56,5 +80,9 @@ li {
 }
 a {
   color: #42b983;
+}
+table {
+  align-content: center;
+  horiz-align: center;
 }
 </style>
