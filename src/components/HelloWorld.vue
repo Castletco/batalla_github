6c15931b689bd4026eab8633e4f08fed5aafcecd
Castletco <diegocastillonaranjo@gmail.com>
2020-08-29 12:29:32 -0400

    cambio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,7 +36,7 @@
           <b-badge variant="primary" pill>{{info.public_gists}}</b-badge>
         </b-list-group-item>
         <b-list-group-item class="d-flex justify-content-between align-items-center">
-          Total Score:
+          Total Score 1:
           <b-badge variant="primary" pill>{{info.public_repos + info.followers + info.following + info.public_gists}}</b-badge>
         </b-list-group-item>
       </b-list-group>
