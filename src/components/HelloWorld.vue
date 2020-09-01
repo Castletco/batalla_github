47107be8a648877d9a3593123fb235ff0528b73b
castletco <diegocastillonaranjo@gmail.com>
2020-08-31 21:27:11 -0400

    corrección para hacer funcionar la batalla de github3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,107 @@
 <template>
-  <div class="hello">
-    <p>Look up by id: <input v-model="id" /><button @click="updateResults">Search</button></p>
-    <img src="@/assets/loading.gif" v-if="isLoading" />
-    <p v-if="info && !isLoading">The result of our request: {{ info }}</p>
-  </div>
-  <!--<div class="hello">
-    <b-card
-      title="Player 1"
-      :img-src="valorImg"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-    >
-      <b-input-group>
-        <template v-slot:prepend>
-          <b-input-group-text >Username</b-input-group-text>
-        </template>
-        <b-form-input v-model="userName"></b-form-input>
-        <b-input-group-append>
-          <b-button size="sm" text="Button" variant="success" @click="updateResults">Go</b-button>
-        </b-input-group-append>
-      </b-input-group>
-      <b-list-group>
-        <b-list-group-item class="d-flex justify-content-between align-items-center">
-          Public Repositories:
-          <b-badge variant="primary" pill>{{info.public_repos}}</b-badge>
-        </b-list-group-item>
-        <b-list-group-item class="d-flex justify-content-between align-items-center">
-          Numbers of Followers:
-          <b-badge variant="primary" pill>{{info.followers}}</b-badge>
-        </b-list-group-item>
-        <b-list-group-item class="d-flex justify-content-between align-items-center">
-          Numbers of Following:
-          <b-badge variant="primary" pill>{{info.following}}</b-badge>
-        </b-list-group-item>
-        <b-list-group-item class="d-flex justify-content-between align-items-center">
-          Public Gists:
-          <b-badge variant="primary" pill>{{info.public_gists}}</b-badge>
-        </b-list-group-item>
-        <b-list-group-item class="d-flex justify-content-between align-items-center">
-          Total Score:
-          <b-badge variant="primary" pill>{{info.public_repos + info.followers + info.following + info.public_gists}}</b-badge>
-        </b-list-group-item>
-      </b-list-group>
-    </b-card>
-  </div>-->
+  <b-container>
+    <b-row class="justify-content-center">
+      <b-card
+        title="Player 1"
+        :img-src="valorImg"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="m-2"
+      >
+        <b-input-group>
+          <template v-slot:prepend>
+            <b-input-group-text >Username</b-input-group-text>
+          </template>
+          <b-form-input v-model="userName">{{userName}}</b-form-input>
+          <b-input-group-append>
+            <b-button size="sm" text="Button" variant="success" @click="updateResults">Go</b-button>
+          </b-input-group-append>
+        </b-input-group>
+        <b-list-group v-if="info != null">
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Public Repositories:
+            <b-badge variant="primary" pill>{{info.public_repos}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Numbers of Followers:
+            <b-badge variant="primary" pill>{{info.followers}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Numbers of Following:
+            <b-badge variant="primary" pill>{{info.following}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Public Gists:
+            <b-badge variant="primary" pill>{{info.public_gists}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Total Score:
+            <b-badge variant="primary" pill>{{info.public_repos + info.followers + info.following + info.public_gists}}</b-badge>
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+
+      <b-card
+        title="Player 2"
+        :img-src="valorImg2"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="m-2"
+      >
+        <b-input-group>
+          <template v-slot:prepend>
+            <b-input-group-text >Username</b-input-group-text>
+          </template>
+          <b-form-input v-model="userName2">{{userName2}}</b-form-input>
+          <b-input-group-append>
+            <b-button size="sm" text="Button" variant="success" @click="updateResults">Go</b-button>
+          </b-input-group-append>
+        </b-input-group>
+        <b-list-group v-if="info2 != null">
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Public Repositories:
+            <b-badge variant="primary" pill>{{info2.public_repos}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Numbers of Followers:
+            <b-badge variant="primary" pill>{{info2.followers}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Numbers of Following:
+            <b-badge variant="primary" pill>{{info2.following}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Public Gists:
+            <b-badge variant="primary" pill>{{info2.public_gists}}</b-badge>
+          </b-list-group-item>
+          <b-list-group-item class="d-flex justify-content-between align-items-center">
+            Total Score:
+            <b-badge variant="primary" pill>{{info2.public_repos + info2.followers + info2.following + info2.public_gists}}</b-badge>
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+    </b-row>
+    <button @click="battle">BATTLE!</button>
+  </b-container>
 </template>
 <script>
-/*export default {
+export default {
   name: 'HelloWorld',
   data() {
     return {
       info: null,
+      info2: null,
       userName: null,
+      userName2: null,
       valorImg: null,
+      valorImg2: null,
+      totalscore1: 0,
+      totalscore2: 0
+      //isLoading: false    // it's not loading initially
     };
   },
   mounted() {
@@ -63,47 +109,23 @@
   },
   methods: {
     updateResults() {
-      console.log("username:" + this.userName)
       this.getUserData();
     },
     async getUserData() {
       try {
+        //this.isLoading = true;      // we're about to start the call, so that means we're loading
         let response = await this.$http.get(`https://api.github.com/users/${this.userName}`);  // instead of hardcoding a value, let's use whatever has been typed in the input box
-        this.valorImg = response.data.avatar_url
+        this.valorImg = response.data.avatar_url;
         this.info = response.data;
+        this.totalscore1 = this.info.public_repos + this.info.followers + this.info.following + this.info.public_gists;
+        let rs = await this.$http.get(`https://api.github.com/users/${this.userName2}`);
+        this.valorImg2 = rs.data.avatar_url;
+        this.info2 = rs.data;
+        this.totalscore2 = this.info2.public_repos + this.info2.followers + this.info2.following + this.info2.public_gists;
       }
       catch(error) {
         console.log(`Something went wrong: ${error}`);
-      }
-    }
-  }
-}*/
-export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-      info: null,
-      id: 42,
-      isLoading: false    // it's not loading initially
-    };
-  },
-  mounted() {
-    this.getBerryData();
-  },
-  methods: {
-    updateResults() {
-      this.getBerryData();
-    },
-    async getBerryData() {
-      try {
-        this.isLoading = true;      // we're about to start the call, so that means we're loading
-        let response = await this.$http.get(`https://pokeapi.co/api/v2/berry/${this.id}`);
-        this.info = response.data;
-        this.isLoading = false;     // since this is after the await, we've completed the loading
-      }
-      catch(error) {
-        console.log(`Something went wrong: ${error}`);
-        this.isLoading = false;     // we'll still want this regardless
+        //this.isLoading = false;     // we'll still want this regardless
         if (error.response) {
           // here, you may want to drill even further to handle 400 and 500 level errors differently
           console.log(error.response);
@@ -116,6 +138,13 @@ export default {
           // something in the setup of the request triggered an error
           console.log(error.message);
         }
+      }
+    },
+    battle() {
+      if (this.totalscore1 > this.totalscore2) {
+        alert ("el ganador es: " + this.userName);
+      } else {
+        alert ("el ganador es: " + this.userName2);
       }
     }
   }
